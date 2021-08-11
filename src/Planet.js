465b63ab988fbea640286efb040f9bb5894f0b3e
hikangg <kangg1987@hotmail.com>
2021-08-11 21:44:37 +0800

    update planet component

--- src/Planet.js
+++ src/Planet.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const Planet = (props) => {
   return (
-    <div class="grid grid-cols-2 h-32 border border-gray-200 my-4">
-        <div class="relative">
-            <img class="absolute top-0 left-0 w-full h-32 object-cover" src={require('./assets/img/' + props.planetName + '.jpeg').default} alt=""/>
+    <div className="grid grid-item grid-cols-2 h-32 border border-gray-200 my-4">
+        <div className="relative grid-item-content">
+            <img className="absolute top-0 left-0 w-full h-32 object-cover grid-item-content" src={require('./assets/img/' + props.planetName + '.jpeg').default} alt=""/>
         </div>
-        <p class="text-3xl font-bold text-left p-10">{props.planetName.charAt(0).toUpperCase() + props.planetName.slice(1)}</p>
+        <p className="text-3xl font-bold text-left p-10 grid-item-content">{props.planetName.charAt(0).toUpperCase() + props.planetName.slice(1)}</p>
     </div>
   );
 }
